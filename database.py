fe5302be53f533f8b4029165b2eb5a010a38bd30
XXmmRR <Xorex50@protonmail.com>
2024-04-04 01:15:48 +0500

    init database

---
+++ database.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+import databases
+import pydantic
+
+import ormar
+import sqlalchemy
+
+DATABASE_URL = "sqlite:///db.sqlite"
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+class BaseMeta(ormar.ModelMeta):
+    metadata = metadata
+    database = database
+
+class User(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "Users"
+
+    id: int = ormar.Integer()
+    username: int = ormar.String(max_length=100)
+    is_anon: bool = ormar.Boolean(null=True)
+    name: str = ormar.String(max_length=100)
+    
+    
