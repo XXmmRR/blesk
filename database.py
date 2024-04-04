63fd8e10cb93a3bae7c77bfc4531567aa0c8fe86
XXmmRR <Xorex50@protonmail.com>
2024-04-04 12:01:53 +0500

    fix

--- database.py
+++ database.py
@@ -19,8 +19,8 @@ class User(ormar.Model):
 
     id: int = ormar.Integer()
     username: int = ormar.String(max_length=100)
-    is_anon: bool = ormar.Boolean(null=True)
-    number: str = ormar.String(null=True)
+    is_anon: bool = ormar.Boolean(nullable=True)
+    number: str = ormar.String(nullable=True)
     name: str = ormar.String(max_length=100)
     
 metadata.create_all()
