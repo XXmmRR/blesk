7cbd5b18a6b52ddd2548d8a055922ad3902f8005
XXmmRR <Xorex50@protonmail.com>
2024-04-04 12:14:26 +0500

    fix

--- database.py
+++ database.py
@@ -5,22 +5,28 @@ import pydantic
 
 import ormar
 import sqlalchemy
+from sqlalchemy.engine import create_engine
 
 DATABASE_URL = "sqlite:///db.sqlite"
-database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
+database = databases.Database(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 
+class MainMeta(ormar.ModelMeta):
+    metadata = metadata
+    database = database
+    
+    
 class User(ormar.Model):
-    class Meta:
-        metadata=metadata
-        database=database
+    class Meta(MainMeta):
         tablename = "Users"
 
     id: int = ormar.Integer()
     username: int = ormar.String(max_length=100)
     is_anon: bool = ormar.Boolean(nullable=True)
-    number: str = ormar.String(nullable=True)
+    number: str = ormar.String(nullable=True, max_length=100)
     name: str = ormar.String(max_length=100)
     
-metadata.create_all()
+    
+metadata.create_all(engine)
