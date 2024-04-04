c89cc10b7c877687676dc41d4b50a5df335f71b5
XXmmRR <Xorex50@protonmail.com>
2024-04-04 11:30:38 +0500

    fix keyboard

--- keyboard/keyboards.py
+++ keyboard/keyboards.py
@@ -1,13 +1,15 @@
 from aiogram import types
 
-start_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-start_keyboard.add(types.KeyboardButton(text='Поделиться контактом', request_contact=True))
-start_keyboard.add(types.KeyboardButton(text='Анонимно'))
+start_buttons = [ 
+  [types.KeyboardButton(text='Поделиться контактом', request_contact=True)],
+  [types.KeyboardButton(text='Анонимно')]
+]
+start_keyboard = types.ReplyKeyboardMarkup(keyboard=start_buttons, resize_keyboard=True)
 
 
-main_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
 main_keyboard_list = ['Предупредить о риске', 'Рассказать о проблеме', 'Предложить идею', 'Попросить о помощи', 'Поблагодарить кого-то']
 
-[main_keyboard.add(types.KeyboardButton(text=x)) for x in main_keyboard_list]
+main_buttons = [[types.KeyboardButton(text=x) for x in main_keyboard_list]]
+main_keyboard = types.ReplyKeyboardMarkup(keyboard=main_buttons)
 
